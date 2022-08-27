164cdf166359f25c60a6b670e78d3e60909a6dc7
Edem <ziddah.edem@gmail.com>
2022-08-27 17:06:40 +0000

    updated

--- src/components/icons/Spinner.vue
+++ src/components/icons/Spinner.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     role="status"
-    :class="`w-${width} h-${height} mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`"
+    :class="`w-7 h-7 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`"
     viewBox="0 0 100 101"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
